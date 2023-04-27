016d52bac33e1895f842422f3d9b64dcd92d7174
matthew.murray <matthewmurray14@gmail.com>
2023-04-27 07:30:33 -0500

    First branch of exercise

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,45 @@
-﻿namespace SelectionStatementExercise
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
+            //This here is the assignment in its basic form. Will create branches to expand on this.
         {
-            Console.WriteLine("Hello, World!");
+            var favNumber = 8;
+
+            Console.WriteLine("Let's play a game. Try and guess my favorite number.");
+
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput == favNumber)
+            {
+                Console.WriteLine("Dude! thats it!");
+
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("Nope! Thats too high.");
+            }
+            else
+            {
+                Console.WriteLine("Nope! Too low");
+            }
+
+            
+            
+
+            
+
+  
+            
+
+
+            
+
+
+
         }
     }
 }
